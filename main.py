224ae1fcec6798de4efcd866b3ccf23b61930b6a
freshinski <max.rast96@gmail.com>
2025-12-21 13:05:59 +0100

    Implement FPS limiting and dt

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+
+def main():    
+    pygame.init()
+    screen = pygame.display.set_mode ((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+            log_state()
+            for event in pygame.event.get(): 
+                if event.type == pygame.QUIT:
+                    return
+            dt = clock.tick(60) / 1000
+            
+    screen.fill("black")
+    pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
