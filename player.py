6eafd54fdf8eb5a8e3c0d47b5e4bcbfe18104f67
freshinski <max.rast96@gmail.com>
2025-12-30 20:49:12 +0100

    making the triangle rotate

--- player.py
+++ player.py
@@ -1,12 +1,15 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
-from constants import LINE_WIDTH
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y,):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -20,3 +23,10 @@ class Player(CircleShape):
        
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
         pass
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.key_d]:
+            self.rotate(dt)
